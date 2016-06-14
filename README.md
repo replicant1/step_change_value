89283c3e078cb94c2d8968647c5e523f8ea9f1ef
Kirill Ashikhmin <ka@binwell.com>
2016-06-14 16:58:21 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,22 @@
+## Change Value - Change value in file step for ([BITRISE](https://www.bitrise.io))
+
+Change value in file. (e.g. constants)
+
+### How to use this Step
+
+Can be run directly with the [bitrise CLI](https://github.com/bitrise-io/bitrise),
+just `git clone` this repository, `cd` into it's folder in your Terminal/Command Line
+and call `bitrise run test`.
+
+*Check the `bitrise.yml` file for required inputs which have to be
+added to your `.bitrise.secrets.yml` file!*
+
+
+### Change log
+
+Version 1.0.0
+* Initial commit.
+
+### Credits
+
+Thanks to Bitrise team for this awesome service / prompt support!
---
+++ step.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# exit if a command fails
+set -e
+#
+# Required parameters
+if [ -z "${file}" ] ; then
+  echo " [!] Missing required input: file"
+  exit 1
+fi
+if [ ! -f "${file}" ] ; then
+  echo " [!] File doesn't exist at specified path: ${file}"
+  exit 1
+fi
+if [ -z "${old_value}" ] ; then
+  echo " [!] No old_value specified!"
+  exit 1
+fi
+if [ -z "${new_value}" ] ; then
+  echo " [!] No new_value specified!"
+  exit 1
+fi
+
+
+# ---------------------
+# --- Configs:
+
+echo " (i) Provided file path: ${file}"
+echo " (i) Provided old_value: ${old_value}"
+echo " (i) Provided new_value: ${new_value}"
+
+if [ "${show_file}" == "true" ]; then
+echo "------------------------------------------"
+echo "------------------------------------------"
+echo "------------------------------------------"
+echo "-------------OLD  FILE--------------------"
+echo "------------------------------------------"
+echo "------------------------------------------"
+echo "------------------------------------------"
+cat ${file}
+fi
+
+# ---------------------
+# --- Main:
+
+# verbose / debug print commands
+#set -v
+sed -i -e 's/'"${old_value}"'/'"${new_value}"'/g' ${file}
+
+if [ "${show_file}" == "true" ]; then
+echo "------------------------------------------"
+echo "------------------------------------------"
+echo "------------------------------------------"
+echo "-------------NEW  FILE--------------------"
+echo "------------------------------------------"
+echo "------------------------------------------"
+echo "------------------------------------------"
+cat ${file}
+fi
+
+# ==> Bundler version patched in Info.plist file for iOS project
